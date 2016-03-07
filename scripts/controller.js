083ade56f7edbfe76403306953303e3f65f128fe
Loquacious <Loquacious@users.noreply.github.com>
2016-03-07 10:50:34 -0500

    Unobtrusive JavaScript

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,4 +1,15 @@
 //This makes the play button work.
 window.onload = function () {
-	document.getElementById("play").onclick = function(){arshsim();};
+	var header = document.getElementsByClassName('page-header')[0],
+		play = document.createElement('a'),
+		beta = document.createElement('sup');
+	play.className = 'btn';
+	play.innerHTML = 'Play Game';
+	play.onclick = function () {
+		arshsim();
+		return false;
+	};
+	beta.innerHTML = 'BETA';
+	play.appendChild(beta);
+	header.appendChild(play);
 };
