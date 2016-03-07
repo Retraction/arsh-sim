22dafc672ae7d7d7a46b32b5eaccafbf1a08b193
Loquacious <Loquacious@users.noreply.github.com>
2016-03-07 11:34:10 -0500

    Rearranged variables

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,15 +1,15 @@
 //This makes the play button work.
 window.onload = function () {
-	var header = document.getElementsByClassName('page-header')[0],
+	var beta = document.createElement('sup'),
 		play = document.createElement('a'),
-		beta = document.createElement('sup');
+		header = document.getElementsByClassName('page-header')[0];
+	beta.innerHTML = 'BETA';
 	play.className = 'btn';
 	play.innerHTML = 'Play Game';
 	play.onclick = function () {
 		arshsim();
 		return false;
 	};
-	beta.innerHTML = 'BETA';
 	play.appendChild(beta);
 	header.appendChild(play);
 };
